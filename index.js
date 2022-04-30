3622b6b01318d7c87c8397b920d6f2d5881265f5
Malik <malikspruill@gmail.com>
2022-04-30 15:59:17 -0400

    Added additional routing with file syncing to update json data with newly created notes

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3005;
+const fs = require("fs");
+const path = require("path");
 const notes = require("./db/db");
 
-app.get("/", (req, res) => res.json(notes));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+
+app.post("/", (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+    console.log(req.body);
+    notes.push(req.body);
+    fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes, null, 2));
+    res.json(req.body);
+});
 
 app.listen(PORT, () => console.log("The server has started"));
+
+
