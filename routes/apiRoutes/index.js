26375cf52b1e094eef8435a7168e497cd4105102
Malik <malikspruill@gmail.com>
2022-05-01 02:37:16 -0400

    Added functionality to delete notes from the saved json sub-directory as well as the client-side column saved note section

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,12 @@
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
-const noteData = require("../../db/db.json");
+let noteData = require("../../db/db.json");
 const uuid = require("uuid");
 
-router.get("/", (req, res) => {
-    res.json(noteData);
-})
-
 //reads db.json file and returns saved notes as JSON
-router.get("/notes", (req,res) => {
+router.get("/notes", (req, res) => {
     res.json(noteData);
-    
 })
 
 // allows for new notes to be posted in the left saved notes column and saved in db.json file
@@ -19,11 +14,24 @@ router.post("/notes", (req, res) => {
     if (!req.body) {
         return res.sendStatus(400);
     }
-    console.log(req.body);
+
     req.body.id = uuid.v4();
     noteData.push(req.body);
     fs.writeFileSync(path.join(__dirname, "../../db/db.json"), JSON.stringify(noteData, null, 2));
     res.json(req.body);
 });
+
+// allows for notes to be deleted in both the saved notes column and the db.json file
+router.delete("/notes/:id", (req, res) => {
+    let deletedNote = noteData.some(note => note.id === req.params.id);
+
+    if (deletedNote) {
+        noteData = noteData.filter(note => note.id !== req.params.id)
+    } else {
+        res.sendStatus(400);
+    }
+    fs.writeFileSync(path.join(__dirname, "../../db/db.json"), JSON.stringify(noteData, null, 2));
+    res.json(noteData);
+})
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const path = require("path");
 
+//Allows routing to specific html webpages
 router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../../public/index.html")));
 router.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "../../public/notes.html")));
 router.get("*", (req, res) => res.sendFile(path.join(__dirname, "../../public/index.html")));
